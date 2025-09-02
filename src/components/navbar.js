8266448bd24b6c820aec423e47754a5aa245a4c5
Pawan Mayekar <pawanmws11@gmail.com>
2025-09-02 15:02:04 +0530

    Uncomment resume download button in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -82,21 +82,21 @@ const Navbar = ({ theme, toggleTheme }) => {
             ))}
           </motion.div>
 
-          // <motion.a
-          //   href={resumePdf}
-          //   download="Pawan_Mayekar_Resume.pdf"
-          //   className="nav-resume-btn"
-          //   whileHover={{ scale: 1.05 }}
-          //   whileTap={{ scale: 0.95 }}
-          //   initial={{ opacity: 0, y: -20 }}
-          //   animate={{ opacity: 1, y: 0 }}
-          //   transition={{ duration: 0.3, delay: 0.6 }}
-          // >
-          //   <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
-          //     <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-          //   </svg>
-          //   Resume
-          // </motion.a>
+          <motion.a
+            href={resumePdf}
+            download="Pawan_Mayekar_Resume.pdf"
+            className="nav-resume-btn"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.6 }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+            </svg>
+            Resume
+          </motion.a>
 
           <motion.button
             className="theme-toggle"
